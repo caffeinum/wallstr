79a681550f6b82ffdbe1b6de205d9fd830c0644d
Guro Bokum <jiojiajiu@gmail.com>
2025-01-09 16:38:58 +0300

    chore(backend): add conf for dyvy

---
+++ packages/backend/dyvy/conf.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Env(str, Enum):
+    dev = "dev"
+    prod = "prod"
+
+
+class Settings(BaseSettings):
+    ENV: Env = Env.dev
+    DEBUG: bool = False
+
+    # required
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", case_sensitive=True, extra="ignore"
+    )
+
+
+# https://github.com/pydantic/pydantic/issues/3753
+settings = Settings.model_validate({})
