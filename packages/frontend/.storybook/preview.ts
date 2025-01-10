483b330330103d6131732f4286aaf22f3f7a363a
Guro Bokum <jiojiajiu@gmail.com>
2025-01-10 15:45:07 +0300

    build(frontend): add storybook

---
+++ packages/frontend/.storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from "@storybook/react";
+
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+import "@/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    viewport: {
+      viewports: MINIMAL_VIEWPORTS,
+    },
+  },
+};
+
+export default preview;
