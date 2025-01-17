7195510edb9c1cbe667ad2fa3c8de6c3ac7aa0e0
Guro Bokum <jiojiajiu@gmail.com>
2025-01-17 04:51:51 +0300

    feat(ui): extend FooterLines

--- packages/frontend/src/components/footer/FooterLine.tsx
+++ packages/frontend/src/components/footer/FooterLine.tsx
@@ -1,6 +1,6 @@
 import {FaXTwitter} from "react-icons/fa6";
 
-const footerLines = [GithubLine, XLine];
+const footerLines = [GithubLine, XLine, DyvyLine, YearLine];
 
 export default function FooterLine() {
   const index = Math.round(Math.random() * (footerLines.length - 1));
@@ -43,4 +43,37 @@ function XLine() {
       <FaXTwitter className="w-4 h-4 relative -top-0.5" />
     </a>
   );
+}
+
+function DyvyLine() {
+  return (
+    <a
+      href="https://x.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="link link-hover inline-flex justify-center gap-1"
+    >
+      <span>Built with</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        aria-hidden="true"
+        className="inline-block h-4 w-4 relative"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+        ></path>
+      </svg>
+      <span>by Dyvy</span>
+    </a>
+  );
+}
+
+function YearLine() {
+  return <span>2025</span>;
 }
