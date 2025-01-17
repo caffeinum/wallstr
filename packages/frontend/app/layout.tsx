7d8ed2533bac553fc70dded52c8aa89de2648ad3
Guro Bokum <jiojiajiu@gmail.com>
2025-01-18 02:24:29 +0300

    chore(frontend): add tanstack-query

--- packages/frontend/app/layout.tsx
+++ packages/frontend/app/layout.tsx
@@ -1,5 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
+
+import QueryClientProvider from "@/providers/QueryClientProvider";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <QueryClientProvider>{children}</QueryClientProvider>
       </body>
     </html>
   );
---
+++ packages/frontend/src/providers/QueryClientProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+import {QueryClient, QueryClientProvider as TanstackQueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+export default function QueryClientProvider({children}: {children: React.ReactNode}) {
+  return (
+    <TanstackQueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools buttonPosition="bottom-left" />
+    </TanstackQueryClientProvider>
+  );
+}
